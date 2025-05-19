24c2fae33f1fb33bc6a660c494660af1a07a8fd0
bovin <bovin@DESKTOP-1KSL3R7>
2025-05-19 16:09:54 +0530

    Authorization added

---
+++ auth.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // This is where you would typically save the user to your database
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const express = require('express');
+const session = require('express-session'); 
+const passport = require('passport');
+require('dotenv').config(); // Add this line
+
+require('./auth'); // This loads your auth configuration
 
 const app = express();
+
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ 
+    secret: process.env.SESSION_SECRET || "cat", // Better to use env variable
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
     res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
-app.get('/protected', (req,res) =>{
-    res.send('Hello!')
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+app.get('/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/protected',
+        failureRedirect: '/auth/failure',
+    })
+);
+
+app.get('/protected', isLoggedIn, (req, res) => {
+    res.send(`Hello ${req.user.displayName}`);
 });
 
-app.listen(5000, ()=> console.log('Listning on:5000'));
+app.get('/auth/failure', (req, res) => {
+    res.send('Something went wrong..');
+});
+
+app.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        req.session.destroy((err) => {
+            if (err) { return next(err); }
+            res.send('Goodbye!');
+        });
+    });
+});
+
+app.listen(5000, () => console.log('Listening on: 5000'));
